27a9e6e437b346059f017dc1bad0fe37aa12668a
binshi <shibinpaul@gmail.com>
2019-08-27 07:47:57 +0530

    and, xor

---
+++ starter/ch10/perceptron_and.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from ch10.nn.perceptron import Perceptron
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [0], [0], [1]])
+
+print("[INFO] training perceptron...")
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+
+print("[INFO] testing perceptron...")
+for (x, target) in zip(X, y):
+    pred = p.predict(x)
+    print("[INFO] data={}, ground-truth={}, pred={}".format(x, target[0], pred))
