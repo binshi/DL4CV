832b659b2df0a33ee784cbdfde67d402c78d7b93
binshi <shibinpaul@gmail.com>
2019-08-27 16:28:04 +0530

    m nist and cifar10

---
+++ starter/ch10/keras_cifar10.py
@@ -0,0 +1,52 @@
+from keras.datasets import cifar10
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+from sklearn.datasets import fetch_openml
+
+print("[INFO] loading CIFAR-10 data...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+trainX = trainX.reshape((trainX.shape[0], 3072))
+testX = testX.reshape((testX.shape[0], 3072))
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072,), activation="relu"))
+model.add(Dense(512, activation="relu"))
+model.add(Dense(10, activation="softmax"))
+
+print("[INFO] training network...")
+sgd = SGD(0.01)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+H = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=100, batch_size=32)
+
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=labelNames))
+
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(np.arange(0, 100), H.history['loss'], label='train_loss')
+plt.plot(np.arange(0, 100), H.history['val_loss'], label='val_loss')
+plt.plot(np.arange(0, 100), H.history['acc'], label='train_acc')
+plt.plot(np.arange(0, 100), H.history['val_acc'], label='val_acc')
+plt.title('Training Loss & Accuracy')
+plt.xlabel('Epoch #')
+plt.ylabel('Loss/Accuracy')
+plt.legend()
+# plt.savefig(args['output'])
+plt.savefig('cifar10')
---
+++ starter/ch10/nn_mnist.py
@@ -0,0 +1,22 @@
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+
+from ch10.nn.neuralnetwork import NeuralNetwork
+
+print("[INFO] loading MNIST (sample) dataset...")
+
+digits = datasets.load_digits()
+data = digits.data.astype("float")
+data = (data - data.min()) / (data.max() - data.min())
+print("[INFO] samples: {}, dim: {}".format(data.shape[0], data.shape[1]))
+
+(trainX, testX, trainY, testY) = train_test_split(data, digits.target, test_size=0.25)
+trainY = LabelBinarizer().fit_transform(trainY)
+testY = LabelBinarizer().fit_transform(testY)
+
+print("[INFO] training network...")
+nn = NeuralNetwork([trainX.shape[1], 32, 16, 10])
+print("[INFO] {}".format(nn))
+nn.fit(trainX, trainY, epochs=1000)
