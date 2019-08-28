46d96f4f1295146591440bb73b85784ad48de419
binshi <shibinpaul@gmail.com>
2019-08-28 16:37:49 +0530

    Shallownet-animals

--- starter/ch12/shallownet_animals.py
+++ starter/ch12/shallownet_animals.py
@@ -1,5 +1,7 @@
 from imutils import paths
 from keras.optimizers import SGD
+import numpy as np
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 
@@ -7,6 +9,7 @@ from ch07.utilities.datasets import SimpleDatasetLoader
 from ch07.utilities.preprocessing import SimplePreprocessor
 from ch12.utilities.nn.conv.shallownet import ShallowNet
 from ch12.utilities.preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
+import matplotlib.pyplot as plt
 
 print("[INFO] loading images...")
 imagePaths = list(paths.list_images("../../../DL4CVStarterBundle/Datasets/animals"))
@@ -29,3 +32,20 @@ model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy
 
 print("[INFO] training network...")
 H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
+
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["cat", "dog", "panda"]))
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
+
