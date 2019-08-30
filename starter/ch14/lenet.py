f4c95b73fc133d6c0f8f29127d5852b413aeff9a
binshi <shibinpaul@gmail.com>
2019-08-30 14:45:55 +0530

    LeNet-mnist

--- starter/ch14/lenet.py
+++ starter/ch14/lenet.py
@@ -1,6 +1,7 @@
 from keras import Sequential
 from keras import backend as K
-from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense
+from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
+
 
 class LeNet:
     @staticmethod
@@ -11,17 +12,44 @@ class LeNet:
         if K.image_data_format()=="channels_first":
             inputShape=(depth,height,width)
 
-        model.add(Conv2D(20, (5, 5), padding="same",input_shape=inputShape))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(20, (5, 5), padding="same"))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # model.add(Conv2D(20, (5, 5), padding="same",input_shape=inputShape))
+        # model.add(Activation("relu"))
+        # model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        # model.add(Conv2D(20, (5, 5), padding="same"))
+        # model.add(Activation("relu"))
+        # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # model.add(Flatten())
+        # model.add(Dense(500))
+        # model.add(Activation("relu"))
+        # model.add(Dense(classes))
+        # model.add(Activation("softmax"))
+        # return model
+
+        model.add(Conv2D(32, (3, 3), input_shape=(28, 28, 1)))
+        model.add(BatchNormalization(axis=-1))
+        model.add(Activation('relu'))
+        model.add(Conv2D(32, (3, 3)))
+        model.add(BatchNormalization(axis=-1))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, (3, 3)))
+        model.add(BatchNormalization(axis=-1))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(BatchNormalization(axis=-1))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
         model.add(Flatten())
-        model.add(Dense(500))
-        model.add(Activation("relu"))
-        model.add(Dense(classes))
-        model.add(Activation("softmax"))
+
+        # Fully connected layer
+        model.add(Dense(512))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(10))
+
+        model.add(Activation('softmax'))
+
         return model
-
-
